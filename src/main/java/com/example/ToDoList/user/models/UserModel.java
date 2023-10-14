8e211a6daa497da6fe3a17f8147e079a0b7c765f
MarcelinoGarcia-NA <marcelino.garcia@novaandradina.org>
2023-10-14 19:23:22 -0300

    configureted h2 and jpa, for created of entity

--- src/main/java/com/example/ToDoList/user/models/UserModel.java
+++ src/main/java/com/example/ToDoList/user/models/UserModel.java
@@ -1,13 +1,26 @@
 package com.example.ToDoList.user.models;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
     private UUID id;
     private String name;
     private String email;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
