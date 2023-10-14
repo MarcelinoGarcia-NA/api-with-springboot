4129be9137d161ae1cbb4ceb77997f3907c15db1
MarcelinoGarcia-NA <marcelino.garcia@novaandradina.org>
2023-10-14 18:49:33 -0300

    submit project for github

---
+++ src/main/java/com/example/ToDoList/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.example.ToDoList.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+    @GetMapping("/")
+    public ResponseEntity<String> create(){
+        String hello = "olá mundo!";
+        return ResponseEntity.status(HttpStatus.CREATED).body(hello);
+    }
+}
