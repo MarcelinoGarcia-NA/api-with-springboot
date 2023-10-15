47b6d9f3318b5262c2403bb6b8a68c2e1b66715b
MarcelinoGarcia-NA <marcelino.garcia@novaandradina.org>
2023-10-15 15:12:41 -0300

    creating filter for user auth register task

--- src/main/java/com/example/ToDoList/task/models/TaskModel.java
+++ src/main/java/com/example/ToDoList/task/models/TaskModel.java
@@ -33,6 +33,8 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime endAt;
 
+    private UUID idUser;
+    
     @CreationTimestamp
     private LocalDateTime createdAt;
 }
