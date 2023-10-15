b733faa99c3d7b116897d6a3d871ee410baa47b1
MarcelinoGarcia-NA <marcelino.garcia@novaandradina.org>
2023-10-15 20:41:56 -0300

    create utils e function put

--- src/main/java/com/example/ToDoList/task/controllers/TaskController.java
+++ src/main/java/com/example/ToDoList/task/controllers/TaskController.java
@@ -1,6 +1,5 @@
 package com.example.ToDoList.task.controllers;
 
-import java.net.http.HttpRequest;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -8,13 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ToDoList.task.interfaces.ITaskRepository;
 import com.example.ToDoList.task.models.TaskModel;
+import com.example.ToDoList.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -45,5 +47,24 @@ public class TaskController {
         }
         var taskCreated = this.taskRepository.save(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+      @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){  
+        
+        var task = this.taskRepository.findById(id).orElse(null);
+      
+
+        if(task == null){
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Atividade não encontrada!");
+        }
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+           return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para esta operação!");
+        }
+
+        Utils.copyNonNullProprieties(taskModel, task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(taskUpdated);
     }
 }
